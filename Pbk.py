42cb120b827bae9ad0961e9aedba8b37159f60b5
Afzal Shadab <mashadab@mit.edu>
2022-01-01 09:46:26 -0600

    Working last

--- Pbk.py
+++ Pbk.py
@@ -4,42 +4,41 @@
 Pbk solution
 """
 
-import scipy.integrate as integrate
-import scipy.special as special
-import numpy as np
+from scipy.integrate import quad
+from scipy.special import ellipk
+from numpy import sin, cos, sqrt, array, pi, nan, inf, savetxt, linspace, isnan
 from scipy.optimize import fsolve,least_squares
-import math
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure, xlabel, ylabel, plot, vlines, hlines, savefig
 
 def L_res_func(L,alpha, beta, C):
- return L - C*integrate.quad(lambda phi: special.ellipk(alpha + (beta - alpha)* np.sin(phi)**2)/np.sqrt(1 - alpha - (beta - alpha)* np.sin(phi)**2), 0, np.pi/2)[0]
+ return L - C*quad(lambda phi: ellipk(alpha + (beta - alpha)* sin(phi)**2)/sqrt(1 - alpha - (beta - alpha)* sin(phi)**2), 0, pi/2)[0]
 
 def H_res_func(H,alpha, beta,C):
- return H - C*np.sqrt(alpha)*integrate.quad(lambda phi: (special.ellipk(alpha * np.sin(phi)**2)*np.sin(phi))/np.sqrt((1 - alpha* np.sin(phi)**2)*(beta - alpha* np.sin(phi)**2)), 0, np.pi/2)[0]
+ return H - C*sqrt(alpha)*quad(lambda phi: (ellipk(alpha * sin(phi)**2)*sin(phi))/sqrt((1 - alpha* sin(phi)**2)*(beta - alpha* sin(phi)**2)), 0, pi/2)[0]
 
 def H0_func(alpha, beta,C):
- return C*integrate.quad(lambda phi: (special.ellipk(np.cos(phi)**2)*np.sin(phi)*np.cos(phi))/np.sqrt((1 - (1-alpha)* np.sin(phi)**2)*(1 - (1-beta)* np.sin(phi)**2)), 0, np.pi/2)[0]
+ return C*quad(lambda phi: (ellipk(cos(phi)**2)*sin(phi)*cos(phi))/sqrt((1 - (1-alpha)* sin(phi)**2)*(1 - (1-beta)* sin(phi)**2)), 0, pi/2)[0]
 
 def H1_res_func(H,alpha, beta,C):
- return 1 - H - H0_func(alpha, beta,C) - C*integrate.quad(lambda phi: (special.ellipk(np.cos(phi)**2)*np.sin(phi))/np.sqrt((1 - alpha* np.sin(phi)**2)*(1 - beta* np.sin(phi)**2)), 0, np.pi/2)[0]
+ return 1 - H - H0_func(alpha, beta,C) - C*quad(lambda phi: (ellipk(cos(phi)**2)*sin(phi))/sqrt((1 - alpha* sin(phi)**2)*(1 - beta* sin(phi)**2)), 0, pi/2)[0]
 
 def full_equations(p):
     alpha, beta, C = p
     return (L_res_func(L,alpha, beta,C), H_res_func(H,alpha, beta,C),H1_res_func(H,alpha, beta,C))
 
 def x_func(L,alpha, beta,C,Psi):
- return L - C*integrate.quad(lambda phi: (special.ellipk(np.sin(phi)**2)*np.sin(phi))/np.sqrt((1 - alpha* np.sin(phi)**2)*(1 - beta* np.sin(phi)**2)), 0, Psi)[0]
+ return L - C*quad(lambda phi: (ellipk(sin(phi)**2)*sin(phi))/sqrt((1 - alpha* sin(phi)**2)*(1 - beta* sin(phi)**2)), 0, Psi)[0]
 
 def z_func(H,alpha, beta,C,Psi):
- return H + H0_func(alpha, beta,C) + C*integrate.quad(lambda phi: (special.ellipk(np.cos(phi)**2)*np.sin(phi))/np.sqrt((1 - alpha* np.sin(phi)**2)*(1 - beta* np.sin(phi)**2)), 0, Psi)[0]
+ return H + H0_func(alpha, beta,C) + C*quad(lambda phi: (ellipk(cos(phi)**2)*sin(phi))/sqrt((1 - alpha* sin(phi)**2)*(1 - beta* sin(phi)**2)), 0, Psi)[0]
 
 def x_res_func(L,alpha, beta,C,Psi,x):
-    return x - L + C*integrate.quad(lambda phi: (special.ellipk(np.sin(phi)**2)*np.sin(phi))/np.sqrt((1 - alpha* np.sin(phi)**2)*(1 - beta* np.sin(phi)**2)), 0, Psi)[0]#- x_func(L,alpha, beta,C,Psi)
+    return x - L + C*quad(lambda phi: (ellipk(sin(phi)**2)*sin(phi))/sqrt((1 - alpha* sin(phi)**2)*(1 - beta* sin(phi)**2)), 0, Psi)[0]#- x_func(L,alpha, beta,C,Psi)
 
 ##########################################
 #Enter H and L values
-H = 0.0001     #lake level
-L = 1     #length of reservoir
+H = 0.5     #lake level
+L = 0.5     #length of reservoir
 n = 2001    #number of points for free surface profile
 ##########################################
 
@@ -63,7 +62,7 @@ print("Free surface profiles")
 print("======================")
 print("Psi \t \t x \t \t z")
 '''
-x_array = np.linspace(0,L,n)
+x_array = linspace(0,L,n)
 for x in x_array:
     def full_equations_new(p):
         Psi = p
@@ -73,7 +72,7 @@ for x in x_array:
 
     print(res_new.x[0],x,z_func(H,res.x[0], res.x[1],res.x[2],res_new.x))
 '''
-Psi_array = np.linspace(0,10,n)
+Psi_array = linspace(0,10,n)
 x_array = []
 z_array = []
 xz_array = []
@@ -84,28 +83,27 @@ for i in range(0,n):
     if x==L: z = H + H0_func(res.x[0],res.x[1],res.x[2]) 
     xz_array.append([x,z])
     x_array.append(x)
-    if i%(int(n/20)) == 0 and x != np.inf and x!=-np.inf:    
+    if i%(int(n/20)) == 0 and x != inf and x!=-inf:    
         print(Psi_array[i],'\t',x,'\t','\t','\t','\t', z)
-x_array  = np.array(x_array)
-xz_array = np.array(xz_array)
-xz_array[x_array<0,:] = np.nan
-xz_array[x_array>L,:] = np.nan
-#xz_array = xz_array[~np.isnan(xz_array).any(axis=1),:]
-#xz_array = xz_array[~np.isinf(xz_array).any(axis=1),:]
+x_array  = array(x_array)
+xz_array = array(xz_array)
+xz_array[x_array<0,:] = nan
+xz_array[x_array>L,:] = nan
+#xz_array = xz_array[~isnan(xz_array).any(axis=1)]
 
-fig = plt.figure(figsize=(8,8) , dpi=100)
-plt.plot(xz_array[:,0],xz_array[:,1],'b-')
-plt.vlines(0,0,1,colors='blue') 
-plt.vlines(L,0,H+H0_func(res.x[0],res.x[1],res.x[2]),colors='blue')  
-plt.vlines(L,0,H,colors='blue') 
-plt.hlines(H,L,1.1*L,colors='blue')   
-plt.hlines(0,0,1.1*L,colors='blue')   
-plt.xlabel('x')
-plt.ylabel('z')
+fig = figure(figsize=(8,8) , dpi=100)
+plot(xz_array[:,0],xz_array[:,1],'b-')
+vlines(0,0,1,colors='blue') 
+vlines(L,0,H+H0_func(res.x[0],res.x[1],res.x[2]),colors='blue')  
+vlines(L,0,H,colors='blue') 
+hlines(H,L,1.1*L,colors='blue')   
+hlines(0,0,1.1*L,colors='blue')   
+xlabel('x')
+ylabel('z')
 H0 = H0_func(res.x[0],res.x[1],res.x[2]) 
 
 
-plt.savefig(f"H{H}_L{L}_H0_{H0}.pdf")
-np.savetxt(f"H{H}_L{L}_H0_{H0}.csv", xz_array, delimiter=",")
+savefig(f"H{H}_L{L}_H0_{H0}.pdf")
+savetxt(f"H{H}_L{L}_H0_{H0}.csv", xz_array, delimiter=",")
 
 
