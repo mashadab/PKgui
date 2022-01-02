eeea9434f78baab59a8082af408cf5ce018074cb
Afzal Shadab <mashadab@mit.edu>
2022-01-01 18:05:30 -0600

    Working

--- GUI/Pbk_GUI.py
+++ GUI/Pbk_GUI.py
@@ -15,10 +15,14 @@ file_list_column = [ [sg.Txt('Length of the aquifer, L')],
            [sg.In(size=(8,4), key='-L-')],
            [sg.Txt('Lake level, H')],
            [sg.In(size=(8,4), key='-H-')],
-           [sg.Txt('Seepage face height, H0')],
+           [sg.Txt('Seepage face height, H0 :')],
            [sg.Txt(size=(20,4), key='-OUTPUT-')  ],
-           [sg.Txt('Alpha')],
+           [sg.Txt('Alpha :')],
            [sg.Txt(size=(20,4), key='-OUTPUT1-')  ],
+           [sg.Txt('Beta :')],
+           [sg.Txt(size=(20,4), key='-OUTPUT2-')  ],
+           [sg.Txt('C :')],
+           [sg.Txt(size=(20,4), key='-OUTPUT3-')  ],
            [sg.Button('Calculate', bind_return_key=True)]]
 
 #for now will only show the name of the chosen file
@@ -49,12 +53,17 @@ while True:
             H0, res, xz_array = PbK_solution(H,L)
             calc = H0
             calc1 = res[0]
-            
+            calc2 = res[1]        
+            calc3 = res[2]
         except:
             calc = 'Invalid H0'
-            calc1 = 'Invalid alpha'
+            calc1 = 'Invalid Alpha'
+            calc2 = 'Invalid Beta'
+            calc3 = 'Invalid C'
 
         window['-OUTPUT-'].update(calc)
         window['-OUTPUT1-'].update(calc1)
+        window['-OUTPUT2-'].update(calc2)
+        window['-OUTPUT3-'].update(calc3)
     else:
         break
